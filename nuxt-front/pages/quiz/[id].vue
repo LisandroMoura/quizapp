687af4f10dfd366d62ae9e5ee9a2915079a17231
lisandromoura <lisandro.cabrom@gmail.com>
2025-02-28 17:26:00 -0300

    Adiciona página de erro "invalid" e exibe informações sobre a data de publicação no quiz

--- nuxt-front/pages/quiz/[id].vue
+++ nuxt-front/pages/quiz/[id].vue
@@ -3,7 +3,9 @@
   <div class="max-w-7xl mx-auto">
     <div class="bg-white shadow-sm rounded-lg p-6">
       <h2 class="text-xl font-semibold mb-4">id:{{ id }} - {{ title }} </h2>
-      <p class="text-gray-600">{{ description }}.</p>
+      <p class="text-gray-600">{{ description }}.  </p>
+      <p><i> Post publicado há {{ diferencialEmDias }} dias </i></p>
+
 
       <div class="mt-6 grid grid-cols-1 gap-5 ">
         <!-- Card 1 -->
@@ -19,13 +21,23 @@
 </template>
 
 <script lang="ts" setup>
-import { QuizForm } from '#components';
 
+const {diasPassados, diferencialEmDias} = useDataEmSegundos()
+
+const data = new Date("2025-01-01");
+
+diasPassados(data);
+
+import { QuizForm } from '#components'; 
 
 // Pegar o ID da rota
 const id: Ref<Number> = ref(Number(useRoute().params.id));
 
 const title: Ref<String> = ref("Qual o seu temperamento?");
+
+  definePageMeta({
+    middleware: "only-numbers",
+  });
 
 // Provisória descrição
 const description: Ref<String> = ref("Faça esse incível teste (quiz) e descubra se o seu temperamento é Colérico, Melancólico, Fleumático ou Sangüíneo.");
