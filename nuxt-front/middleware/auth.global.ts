893ce8714fd6408af7a0d7d37d4041e6360c5908
lisandromoura <lisandro.cabrom@gmail.com>
2025-02-28 17:26:51 -0300

    Adiciona middleware global para verificação de autenticação do usuário

---
+++ nuxt-front/middleware/auth.global.ts
@@ -0,0 +1,16 @@
+// Dedinição de middleware global para todas as rotas...
+
+const logado : boolean= true;
+
+
+// criar middle para verificar se o usuario esta logado
+export default defineNuxtRouteMiddleware((to, from) => {
+
+  if (!logado && to.path !== "/login") {
+    return navigateTo("/login");
+  }
+  // const { $store } = useNuxtApp()
+  // if (!($store as any).state.auth.loggedIn) {
+  //   return navigateTo('/login')
+  // }
+})
