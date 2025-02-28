01a6195051acc226ceaf27b2c2f046106e1706b6
lisandromoura <lisandro.cabrom@gmail.com>
2025-02-28 10:24:02 -0300

    Atualiza links de navegação para caminhos corretos na aplicação

--- nuxt-front/layouts/default.vue
+++ nuxt-front/layouts/default.vue
@@ -16,7 +16,7 @@
                                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
                                 </path>
                             </svg>
-                            <a href="#" class="block">Home</a>
+                            <a href="/" class="block">Home</a>
                         </li>
                         <li class="px-4 py-3 hover:bg-gray-700 rounded-lg mx-2 mb-1 flex items-center">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -25,7 +25,7 @@
                                     d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
                                 </path>
                             </svg>
-                            <a href="#" class="block">Cadastro de Quiz</a>
+                            <a href="/quiz" class="block">Cadastro de Quiz</a>
                         </li>
                         <li class="px-4 py-3 hover:bg-gray-700 rounded-lg mx-2 mb-1 flex items-center">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -34,7 +34,7 @@
                                     d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
                                 </path>
                             </svg>
-                            <a href="#" class="block">Cadastro de Perguntas</a>
+                            <a href="/pregunta" class="block">Cadastro de Perguntas</a>
                         </li>
                         <li class="px-4 py-3 hover:bg-gray-700 rounded-lg mx-2 mb-1 flex items-center">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -43,12 +43,12 @@
                                     d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z">
                                 </path>
                             </svg>
-                            <a href="#" class="block">Cadastro de Resposta</a>
+                            <a href="/resposta" class="block">Cadastro de Resposta</a>
                         </li>
                     </ul>
                 </nav>
                 <div class="p-4 border-t border-gray-700">
-                    <a href="#"
+                    <a href="/logout""
                         class="block py-2 px-4 text-red-400 hover:bg-red-500 hover:text-white rounded-lg flex items-center">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
